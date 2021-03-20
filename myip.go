9c459671c829f1d459a9d251ab426b2c7c48a34f
nmatheka <64628278+nmatheka@users.noreply.github.com>
2021-03-20 09:57:38 -0700

    Update myip.go
    Made the changes.

--- myip.go
+++ myip.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	cloudflare = "http://1.1.1.1/cdn-cgi/trace"
-	ifconfig.me = "http://ifconfig.me"
+	ifconfigMe = "http://ifconfig.me"
 )
 
 // GetIP returns your public IP or an error
@@ -51,22 +51,31 @@ func GetIP() (net.IP, error) {
 	return nil, fmt.Errorf("Unable to find IP")
 }
 
-// GetIPUsingIfConfig_Me uses the ifconfig.me web service to get your public IP. It returns your public IP or an error.
-func GetIPUsingIfConfig_Me() (net.IP, error) {
+// GetIPUsingIfConfigMe uses the ifconfig.me web service to get your public IP. It returns your public IP or an error.
+func GetIPUsingIfConfigMe() (net.IP, error) {
 	resp, err := http.Get(ifconfig.me)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
+	
+	if resp.StatusCode != 200 {
+		err = fmt.Errorf(
+			"Received unexpected status code from ifconfigMe, expected %v but got %v",
+			200,
+			resp.StatusCode,
+		)
+		return nil, err
+	}
+	
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, fmt.Errorf("Unable to read Body")
 	}
 
-	ipS := string(body)
-	ip := net.ParseIP(ipS)
+	ip := net.ParseIP(string(body))
 	if ip == nil {
-		return nil, fmt.Errorf("Unable to parse IP")
+		return nil, fmt.Errorf("Unable to parse IP %q", string(body))
 	}
 	return ip, nil
 

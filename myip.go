12b1778352d71cf2040a6b8bf7a855e2bf4bcd34
nmatheka <64628278+nmatheka@users.noreply.github.com>
2021-04-01 12:47:32 -0700

    Adding suggested changes to myip.go (#3)
    Add ifconfig.me support

--- myip.go
+++ myip.go
@@ -11,6 +11,7 @@ import (
 
 const (
 	cloudflare = "http://1.1.1.1/cdn-cgi/trace"
+	ifconfigMe = "http://ifconfig.me"
 )
 
 // GetIP returns your public IP or an error
@@ -48,4 +49,34 @@ func GetIP() (net.IP, error) {
 	}
 
 	return nil, fmt.Errorf("Unable to find IP")
+}
+
+// GetIPUsingIfConfigMe uses the ifconfig.me web service to get your public IP. It returns your public IP or an error.
+func GetIPUsingIfConfigMe() (net.IP, error) {
+	resp, err := http.Get(ifconfig.me)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	
+	if resp.StatusCode != 200 {
+		err = fmt.Errorf(
+			"Received unexpected status code from ifconfigMe, expected %v but got %v",
+			200,
+			resp.StatusCode,
+		)
+		return nil, err
+	}
+	
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to read Body")
+	}
+
+	ip := net.ParseIP(string(body))
+	if ip == nil {
+		return nil, fmt.Errorf("Unable to parse IP %q", string(body))
+	}
+	return ip, nil
+
 }
